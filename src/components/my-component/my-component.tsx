7c9bc297c15eb4e5a70f471504535702ea21a38d
DwivediShashikant <dwivedi.sk@outlook.com>
2018-10-17 15:19:10 +0530

    add shadow root

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -3,6 +3,7 @@ import { Component, Prop } from '@stencil/core';
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
+  shadow: true
 })
 export class MyComponent {
   @Prop() first: string;
